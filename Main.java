01ac3b82c9116810bbab4add0ace5ff4574a2be7
repl.it user <replituser@example.com>
2020-10-23 09:46:37 +0000

    Assignment1

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hellooo!");
+    for (int i=0; i<200;i++){
+    if  ((i%5 == 0)&&(i!=0)){
+      System.out.println("zahl durch 5 teilbar: "+i);
+    }
+    if ((i-9)%10==0){
+      System.out.println("Zahl endet auf 9: "+i);
+    }
+    if ((i-1+i)%3==0){
+System.out.println("Zahl mit vorgänger % 3: "+i);
+    }    }
   }
 }
